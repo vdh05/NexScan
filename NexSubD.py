cc8266e385b2594734a7258aabe27cc5013a9f4a
hm05 <harshmurjani5114@gmail.com>
2024-08-23 12:29:30 +0530

    Directory Fuzzing

--- NexSubD.py
+++ NexSubD.py
@@ -1,3 +1,13 @@
+"""
+This file is part of NexScan.
+
+NexScan is free software: you can use it for personal and non-commercial purposes under the terms of the Custom License.
+
+For commercial use, please contact Nexeo Security at business@nexeosecurity.tech.
+
+NexScan is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Custom License for more details, located in the LICENSE file.
+"""
+
 import argparse
 import socket
 import re
